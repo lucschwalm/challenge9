caa36d1b8a0bf3172c207b1829445197e415efca
lucschwalm <lucschwalm03@gmail.com>
2023-07-09 21:58:06 -0500

    Program creates a readme file based on user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Title of project:',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter description:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Enter any installation instructions:',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information:',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Enter guidelines for contributors:',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Enter testing instructions:',
+        name: 'testing',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license:',
+        choices: ['GNU GPLv3', 'MIT License', 'No license'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'Enter the email address you would like questions sent to:',
+        name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(fileName, generateMarkdown(data), (err) => {
+        if(err) throw err;
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((value) => {
+            writeToFile('README.md', value);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'GNU GPLv3'){
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if(license === 'MIT License') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'GNU GPLv3'){
+    return 'https://www.gnu.org/licenses/gpl-3.0';
+  } else if(license === 'MIT License') {
+    return 'https://opensource.org/licenses/MIT';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license != 'No license'){
+    return `${license} ${renderLicenseLink(license)}`
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Contribution
+${data.contribution}
+## Tests
+${data.testing}
+## License
+${renderLicenseSection(data.license)}
+## Questions
+GitHub: ${data.username}
+https://github.com/${data.username}
+Please direct questions to: ${data.email}
 `;
 }
 
